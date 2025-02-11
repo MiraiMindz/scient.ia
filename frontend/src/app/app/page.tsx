6c88470936ea5bac76b2bc8fa6536766bcf2dd35
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2025-02-11 13:47:16 -0300

    fixed zustand state persistance

--- frontend/src/app/app/page.tsx
+++ frontend/src/app/app/page.tsx
@@ -6,11 +6,12 @@ import { useEffect } from 'react';
 
 export default function Page() {
     const token = useUserStore((state) => state.token);
+    const rehydrated = useUserStore((state) => state._persist?.rehydrated);
     const router = useRouter();
 
     useEffect(() => {
-        if (!token) { router.push("/"); }
-      }, [router, token]);
+        if (rehydrated && !token) { router.push("/"); }
+    }, [router, token, rehydrated]);
 
     return (
         <main>
--- frontend/src/app/zustandStores/userStore.ts
+++ frontend/src/app/zustandStores/userStore.ts
@@ -2,21 +2,23 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 interface UserState {
-  token: string | null;
-  setToken: (token: string | null) => void;
-  clearToken: () => void;
+    token: string | null;
+    setToken: (token: string | null) => void;
+    clearToken: () => void;
+    _persist?: {
+        rehydrated: boolean;
+    };
 }
 
-// Create the store with persistence.
 export const useUserStore = create<UserState>()(
-  persist(
-    (set) => ({
-      token: null, // Initial state: no token
-      setToken: (token: string | null) => set({ token }),
-      clearToken: () => set({ token: null }),
-    }),
-    {
-      name: 'user-storage',
-    }
-  )
+    persist(
+        (set) => ({
+            token: null, // Initial state: no token
+            setToken: (token: string | null) => set({ token }),
+            clearToken: () => set({ token: null }),
+        }),
+        {
+            name: 'user-storage',
+        }
+    )
 );
